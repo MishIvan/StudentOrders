0030f1e2c33e3a41f96b3f539ac55c56f7a19d68
MishIvan <85023280+MishIvan@users.noreply.github.com>
2023-05-21 10:17:06 +0700

    Зак 2529345. В разработке

---
+++ Ascents/SQLDBHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Ascents
+{
+    internal class BaseDBHelper : IDisposable
+    {
+        protected SqlConnection conn;
+        protected string _errorText;
+        /// <summary>
+        ///  открыта ли БД
+        /// </summary>
+        public bool isOpened { get { return conn.State == System.Data.ConnectionState.Open; } }
+        /// <summary>
+        /// текст ошибки, если ошибки нет - пустое значение
+        /// </summary>
+        public string errorText { get { return _errorText; } }
+
+        /// <summary>
+        /// установление соединения с БД непосредственно в конструкторе
+        /// </summary>
+        public BaseDBHelper()
+        {
+            _errorText = "";
+            String connectionString = AppSettings.Default.ConnectionString;
+            conn = new SqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                _errorText = ex.Message;
+            }
+
+        }
+        /// <summary>
+        /// закрытие соединения
+        /// </summary>
+        public void Dispose()
+        {
+            if (isOpened) conn.Close();
+        }
+
+    }
+    internal class DBHelper : BaseDBHelper
+    {
+        public DBHelper() : base() {}
+    }
+}
