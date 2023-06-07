bbed8bd8d48993dbeb67b68511f941c20343152e
MishIvan <85023280+MishIvan@users.noreply.github.com>
2023-06-07 19:25:34 +0700

    Зак. 2529345. С контрольной работой

---
+++ SoftApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Soft[] arr = new Soft[3];
+            for (int i = 0; i < 3; i++)
+            {
+                arr[i] = new Soft();
+                Console.WriteLine("Введите наименование программного продукта:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("\r\nВведите производителя программного продукта:");
+                string vendor = Console.ReadLine();
+
+                Console.WriteLine("\r\nВведите стоимость программного продукта (0, если ПО сбоводное):");
+                string spricе = Console.ReadLine();
+                double price = 0.0;
+                try
+                {
+                    price = Convert.ToDouble(spricе);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\r\nНеверный формат ввода цены");
+                    return;
+                }
+                if(price < 0.0)
+                {
+                    Console.WriteLine("\r\nЦена не может быть отрицательной");
+                    return;
+                }
+                Console.WriteLine("\r\n");
+                arr[i].SoftName = name;
+                arr[i].SoftVendor = vendor;
+                arr[i].SoftPrice = price;
+            }
+
+            Console.WriteLine("Вы ввели:\r\n");
+            for(int i = 0;i < 3;i++)
+            {
+                Console.WriteLine($"Наименование ПО: {arr[i].SoftName}, Производитель: {arr[i].SoftVendor}, " +
+                    (arr[i].IsFreeSoft ? "Свободное ПО\r\n" : $"Цена: {arr[i].SoftPrice}\r\n"));
+            }
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SoftApp/Soft.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftApp
+{
+    /// <summary>
+    /// Программное обеспечение
+    /// </summary>
+    class Soft
+    {
+        private string m_name;
+        private string m_vendor;
+        private double m_price;
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        public string SoftName
+        {
+            get { return m_name; }
+            set { m_name = value; }
+        }
+        /// <summary>
+        /// Производитель
+        /// </summary>
+        public string SoftVendor
+        {
+            get { return m_vendor; }
+            set { m_vendor = value; }
+        }
+        /// <summary>
+        /// Стоимость
+        /// </summary>
+        public double SoftPrice
+        {
+            get { return m_price; }
+            set { m_price = value; }
+        }
+        /// <summary>
+        /// Свободное ли программное обеспечение
+        /// </summary>
+        public bool IsFreeSoft
+        {
+            get { return m_price == 0.0; }
+        }
+        public Soft()
+        {
+            m_name = string.Empty;
+            m_vendor = string.Empty;
+            m_price = 0.0;
+        }
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="name">Наименование</param>
+        /// <param name="vendor">Производитель</param>
+        /// <param name="price">Цена</param>
+        public Soft(string name, string vendor, double price = 0.0)
+        {
+            m_name = name;
+            m_price = price;
+            m_vendor = vendor;
+        }
+    }
+}
